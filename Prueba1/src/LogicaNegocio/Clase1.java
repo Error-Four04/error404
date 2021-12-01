ee733422ecb92fa9ee1422b21a2a47cc0f41ff9c
notFound <nobody@nothing.com>
2021-12-01 08:54:13 -0600

    YourWelcome

--- Prueba1/src/LogicaNegocio/Clase1.java
+++ Prueba1/src/LogicaNegocio/Clase1.java
@@ -5,5 +5,16 @@ package LogicaNegocio;
  * @author i-linux
  */
 public class Clase1 {
+    private String name = "Adriel";
+    public Clase1() {
+        
+    }
     
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 }
