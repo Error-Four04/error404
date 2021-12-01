3f4ea997ac81a027fc75d56b83074b2495e16bb3
notFound <nobody@nothing.com>
2021-12-01 12:56:24 -0600

    YourWelcome

--- EO_ExamenRamon/src/LogicaNegocio/Estudiante.java
+++ EO_ExamenRamon/src/LogicaNegocio/Estudiante.java
@@ -6,4 +6,45 @@ package LogicaNegocio;
  */
 public class Estudiante {
     
+    private String name;
+    private int age;
+    public Materias listaMaterias = new Materias();
+    
+    public Estudiante(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    //Metodos extra para operaciones de alumno
+    
+    public double promedio() {
+        double promedio = 0;
+        if(listaMaterias.isEmpty())
+            return promedio;
+        for(int i=0; i<listaMaterias.size(); i++) {
+            promedio += listaMaterias.getMateria(i).getNota();
+        }
+        return promedio;
+    }
+    
+    //Agregar materia al estudiante
+    public void addCurse(Materia curse) {
+        listaMaterias.add(curse);
+    }
 }
--- EO_ExamenRamon/src/Presentacion/Main.java
+++ EO_ExamenRamon/src/Presentacion/Main.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Presentacion;
 
+
+//My own imports from my packages
+import LogicaNegocio.MyArrayList;
+import LogicaNegocio.Estudiante;
+import LogicaNegocio.Materia;
+import LogicaNegocio.Materias;
 /**
  *
  * @author i-linux
  */
 public class Main {
     
+    //Creamos el nuestra ArrayList
+    private static MyArrayList<Estudiante> stackStudents = new MyArrayList();
+    
+    public static void main(String[] args) {
+        //Creamos los 3 estudiantes.
+        Estudiante e1 = new Estudiante("Eliot", 24);
+        Estudiante e2 = new Estudiante("John", 20);
+        Estudiante e3 = new Estudiante("Ringo", 18);
+        
+        //agregamos a cada estudiante sus materias.
+        
+        //Para el estudiante Elliot.
+        Materia español1 = new Materia("Español", 80);
+        Materia matematicas1 = new Materia("Matematicas", 100);
+        e1.addCurse(español1);
+        e1.addCurse(matematicas1);
+        
+        //Para el estudiante John.
+        Materia ciberSeguridad2 = new Materia("CiberSeguridad", 100);
+        
+        //Para el estudiante Ringo.
+        Materia derecho3 = new Materia("Derecho",90);
+        Materia civismo3 = new Materia("Civismo", 50);
+        
+        //Agregamos a la pila los estudiantes.
+        stackStudents.push(e1);
+        stackStudents.push(e2);
+        stackStudents.push(e3);
+        
+        //Mostramos por pantalla todos los estudiantes.
+        
+        for(int i=0; i<stackStudents.size(); i++) {
+            String name = stackStudents.get(i).getName();
+            System.out.println("Name: " + name);
+            int age = stackStudents.get(i).getAge();
+            System.out.println("Age: " + age);
+            //Iteramos en la lista Materias.
+            Estudiante e = stackStudents.get(i);
+            double promedio = 0;
+            for(int j=0; j<e.listaMaterias.size(); j++) {
+                Materia m = e.listaMaterias.getMateria(j);
+                System.out.println("-Materia: " + m.getName() + "\n-Calificacion: " + m.getNota() + "\n-estado de la materia: " + m.mensaje() + "\n");
+                promedio += m.getNota();
+            }
+            //obtenemos el promedio.
+            promedio = promedio/e.listaMaterias.size();
+            System.out.println("Promedio Final: " + promedio);
+        }
+    }
 }
